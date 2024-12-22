9199439d102c1653a77a30a68d939fbf3a410fa6
lza_menace <lza_menace@protonmail.com>
2024-12-21 21:01:30 -0800

    dont create wallet, wait for user to upload

--- run_wallet.sh
+++ run_wallet.sh
@@ -2,26 +2,16 @@
 
 
 export DAEMON_URI="${1}"
-export WALLET_CREDS="${2}"
 export WALLET_FILE="/wallet/wallet"
 export PASSWORD_FILE="/wallet/password"
 export INIT_LOG="/wallet/init.log"
 
-# Create new wallet if it doesn't exist
 if [[ ! -f ${WALLET_FILE} ]]; then
-  echo $WALLET_CREDS > ${PASSWORD_FILE}
-  wownero-wallet-cli \
-    --generate-new-wallet ${WALLET_FILE} \
-    --daemon-address ${DAEMON_URI} \
-    --password-file ${PASSWORD_FILE} \
-    --trusted-daemon \
-    --use-english-language-names \
-    --mnemonic-language English \
-    --command status \
-    | tee ${INIT_LOG}
+  echo -e "[!] Wallet file does not exist. Create a new wallet and upload the private view keys to BTCPayServer"
+  sleep 10
+  exit 1
 fi
 
-# Run RPC wallet
 wownero-wallet-rpc \
   --daemon-address ${DAEMON_URI} \
   --wallet-file ${WALLET_FILE} \
@@ -35,3 +25,5 @@ wownero-wallet-rpc \
   --non-interactive \
   --trusted-daemon \
   --tx-notify="/usr/bin/curl -s -X GET http://btcpayserver:49392/wownerolikedaemoncallback/tx?cryptoCode=wow&hash=%s"
+
+sleep 5
